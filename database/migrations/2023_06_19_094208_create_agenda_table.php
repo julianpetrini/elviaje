95812cb01c1f044be7860643f933085a50081c1c
JPET <bwmisojos@gmail.com>
2023-06-19 11:43:21 +0200

    created Agenda migration

---
+++ database/migrations/2023_06_19_094208_create_agenda_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAgendaTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('agenda', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('categoria');
+            $table->date('fecha');
+            $table->time('hora_inicio');
+            $table->time('hora_fin');
+            $table->string('imagen')->nullable();
+            $table->text('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('agenda');
+    }
+};
+
