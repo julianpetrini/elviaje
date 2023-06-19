108d966c650e7a60024d4e5088d9df6d0d7e4f6f
JPET <bwmisojos@gmail.com>
2023-06-19 11:01:38 +0200

    update de texto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
                             <div class="ml-12">
                                 <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                    La aplicacion de viaje mas novedosa del mercado.
+                                    La aplicacion de viaje mas novedosa del mercado nueva.
                                 </div>
                             </div>
                         </div>
