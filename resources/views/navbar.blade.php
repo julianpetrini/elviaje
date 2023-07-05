ad6f8abd890d5cf7627e644f77db0a87837976dc
JPET <bwmisojos@gmail.com>
2023-07-05 17:12:26 +0200

    update. circulo se mueve en menu

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav x-data="{ open: false }" class="bg-transparent text-white dark:bg-gray-800" >
-
+    
 
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
